5f94de6295b4d13f9b8c85013b839201b44fcd57
tandica <williams.tandica@gmail.com>
2021-06-07 18:54:41 +0000

    added styling for day list item component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 import classnames from "classnames";
-import "components/Button.scss";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{props.spots} spots left</h3>
     </li>
   );
 }
