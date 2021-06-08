d86fca255ea2fcf642c7073630994395ed5bd0a3
tandica <williams.tandica@gmail.com>
2021-06-08 13:29:39 -0400

    fixed interviewer list component to add names, avatars and show preselected

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,21 +5,19 @@ import InterviewerListItem from "components/InterviewerListItem";
 export default function InterviewerList(props) {
   const listOfInterviewers = props.interviewers.map((interviewer) => {
     return (
-      <ul key={interviewer.id}>
-        <InterviewerListItem
-          key={interviewer.id}
-          name={interviewer.name}
-          avatar={interviewer.avatar}
-          selected={interviewer.id === props.value}
-          setInterviewer={props.setInterviewer}
-        />
-      </ul>
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
     );
   });
-  return listOfInterviewers;
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{listOfInterviewers}</ul>
+    </section>
+  );
 }
-
-// <section className="interviewers">
-//   <h4 className="interviewers__header text--light">Interviewer</h4>
-//   <ul className="interviewers__list"></ul>
-// </section>;
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -14,7 +14,7 @@ export default function InterviewerListItem(props) {
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   );
 }
