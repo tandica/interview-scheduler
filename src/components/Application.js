279e4200b019a8f842ee51fdfacca2cd23c0e98b
tandica <williams.tandica@gmail.com>
2021-06-08 19:50:14 -0400

    update application.js formatting

--- src/components/Application.js
+++ src/components/Application.js
@@ -95,8 +95,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map(appointment => {
-          return <Appointment key={appointment.id} {...appointment}/>
+        {appointments.map((appointment) => {
+          return <Appointment key={appointment.id} {...appointment} />;
         })}
         <Appointment key="last" time="5pm" />
       </section>
