7b11471ffff1b31dc9b305098559873e6fd409c2
tandica <williams.tandica@gmail.com>
2021-06-08 15:00:47 -0400

    updated first appointment component and interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./styles.scss";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>
+  return <article className="appointment">Appointments</article>
 };
