56d532974ff27aaf03c77425c1b828cb31f2a5e1
tandica <williams.tandica@gmail.com>
2021-06-10 13:13:23 -0400

    updated react test renderer library so tests work

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,4 +1,6 @@
-export function useVisualMode(initialMode) {
+import React, { useState } from "react";
+
+export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
   return { mode };
 }
