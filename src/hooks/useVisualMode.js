833edea44699ee95f2574b339f4d6e3172de53c6
tandica <williams.tandica@gmail.com>
2021-06-10 13:58:35 -0400

    first implementation of back function in custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
-  
-  const transition = function(newMode) {
+  const [history, setHistory] = useState([initialMode]);
 
-    setMode(newMode)
-  }
-  return { mode, transition };
+  const transition = function (newMode) {
+    setMode(newMode);
+  };
+
+  const back = function () {
+    if (history.length <= 1) {
+      return;
+    }
+    history.pop();
+    setMode(history[history.length - 1]);
+    console.log(setMode(history[history.length - 1]));
+  };
+
+  return { mode, transition, back };
 }
