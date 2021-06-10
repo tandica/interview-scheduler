c4e282982945e309604232d974d2dcb9ec8a6a5c
tandica <williams.tandica@gmail.com>
2021-06-10 14:41:22 -0400

    updated appointment index to use custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,20 +3,28 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "../../hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
 
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer.name}
+          interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
       )}
+
     </article>
   );
 }
