debd7eae2376f74b4bc38941059e7e465d932efa
tandica <williams.tandica@gmail.com>
2021-06-10 13:30:00 -0400

    added transition function to custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,5 +2,10 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
-  return { mode };
+  
+  const transition = function(newMode) {
+
+    setMode(newMode)
+  }
+  return { mode, transition };
 }
