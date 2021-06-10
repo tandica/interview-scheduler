190c8d143f6804fb6f0c088fbab8429f1bb00f72
tandica <williams.tandica@gmail.com>
2021-06-10 15:09:04 -0400

    added modes to appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,14 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "../../hooks/useVisualMode";
 
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
-
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
@@ -17,14 +19,20 @@ export default function Appointment(props) {
     <article className="appointment">
       <Header time={props.time} />
 
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && (
+        <Empty
+          onAdd={(() => transition(CREATE), console.log("Clicked onAdd"))}
+        />
+      )}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          interviewer={props.interview.interviewer.name}
         />
       )}
-
+      {mode === CREATE && (
+        <Form interviews={props.interviewers} onCancel={back} />
+      )}
     </article>
   );
 }
