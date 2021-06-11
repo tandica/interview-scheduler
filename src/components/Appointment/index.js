0607217a835b2814deaf4cbad36db66345f6d2be
tandica <williams.tandica@gmail.com>
2021-06-11 12:49:04 -0400

    implemented feature for editing appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -16,7 +16,8 @@ export default function Appointment(props) {
   const CREATE = "CREATE";
   const SAVING = "SAVING";
   const DELETING = "DELETING";
-  const CONFIRM ="CONFIRM"
+  const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
 
   //create an appt
   function save(name, interviewer) {
@@ -27,14 +28,13 @@ export default function Appointment(props) {
     transition(SAVING);
     props.bookInterview(props.id, interview).then(() => transition(SHOW));
   }
-  
+
   //delete an appt
   function cancelInterview(interview) {
-
     transition(DELETING);
     props.cancelInterview(props.id, interview).then(() => transition(EMPTY));
   }
-  
+
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
@@ -49,6 +49,7 @@ export default function Appointment(props) {
           student={props.interview.student}
           interviewer={props.interview.interviewer.name}
           onDelete={() => cancelInterview(props.interview)}
+          onEdit={() => transition(EDIT) }
         />
       )}
       {mode === CREATE && (
@@ -56,7 +57,22 @@ export default function Appointment(props) {
       )}
       {mode === SAVING && <Saving message={SAVING} />}
       {mode === DELETING && <Deleting message={DELETING} />}
-      {mode === CONFIRM && <Confirm onConfirm={cancelInterview} onCancel={back} message={DELETING} />}
+      {mode === CONFIRM && (
+        <Confirm
+          onConfirm={cancelInterview}
+          onCancel={back}
+          message={DELETING}
+        />
+      )}
+      {mode === EDIT && (
+        <Form
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onCancel={back}
+          onSave={save}
+        />
+      )}
     </article>
   );
 }
