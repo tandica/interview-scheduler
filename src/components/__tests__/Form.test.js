06251e6861d840eb61579e2b4d64f33c0553857e
tandica <williams.tandica@gmail.com>
2021-06-14 15:15:28 -0400

    implemented fourth test for form component

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -40,4 +40,22 @@ describe("Form", () => {
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
   });
+
+  it("calls onSave function when the name is defined", () => {
+    /* 1. Create the mock onSave function */
+    const onSave = jest.fn();
+    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
+    const { queryByText } = render(
+      <Form
+        interviewers={interviewers}
+        name="Lydia Miller-Jones"
+        onSave={onSave}
+      />
+    );
+    /* 3. Click the save button */
+    fireEvent.click(queryByText("Save"));
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
 });
