2b92db2a11294ba0d21d9d10202e78dd8b71a0e9
tandica <williams.tandica@gmail.com>
2021-06-16 14:54:10 -0400

    added selector

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,7 +5,9 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.get("li").contains("Tuesday").click();
+    cy.get("li")
+      .contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
   });
-
 });
