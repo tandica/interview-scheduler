a42e9394090f00807e674b816ab1c102e4014ae9
tandica <williams.tandica@gmail.com>
2021-06-16 15:25:04 -0400

    added edit interview test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -18,4 +18,18 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Add]").first().click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Tandica");
+    cy.get('[alt="Tori Malcolm"]').click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Tandica");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 });
